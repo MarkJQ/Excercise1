e51f9c079e252ea74c022d87fdb703fb4403114b
fei liu <fei liu>
2016-09-08 05:08:18 +0200

    First commit

---
+++ www/js/controllers.js
@@ -0,0 +1,94 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('FoodsCtrl', function($scope, $state, $timeout, $ionicFilterBar, Foods) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  var filterBarInstance;
+
+  $scope.showFilterBar = function () {
+    filterBarInstance = $ionicFilterBar.show({
+      items: $scope.foods,
+      update: function (filteredItems, filterText) {
+        $scope.foods = filteredItems;
+        if (filterText) {
+          console.log(filterText);
+        }
+      }
+    });
+  };
+  $scope.add = function(view){
+    $state.go(view);
+  }
+  $scope.refreshFoods = function () {
+    if (filterBarInstance) {
+      filterBarInstance();
+      filterBarInstance = null;
+    }
+ 
+    $timeout(function () {
+      $scope.foods = Foods.all();
+      $scope.$broadcast('scroll.refreshComplete');
+    }, 1000);
+  };
+
+  $scope.foods = Foods.all();
+  $scope.remove = function(food) {
+    Foods.remove(food);
+  };
+})
+
+.controller('AddCtrl', function($scope, $state, $cordovaCamera, Foods){
+  $scope.foods = Foods.all();
+  $scope.newFood = {
+    id: $scope.foods.length,
+    name: '',
+    lastText: '',
+    face: ''
+  }
+
+  $scope.takePhoto = function () {
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
+    };
+
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+      $scope.imgURI = "data:image/jpeg;base64," + imageData;
+      $scope.newFood.face = $scope.imgURI;
+    }, function (err) {
+        // An error occured. Show a message to the user
+    });
+  };
+
+  $scope.confirm = function () {
+    Foods.add($scope.newFood);
+    $scope.close();
+  };
+  $scope.close = function() {
+    $state.go('tab.foods'); 
+  };
+})
+
+.controller('FoodDetailCtrl', function($scope, $stateParams, Foods) {
+  $scope.food = Foods.get($stateParams.foodId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
